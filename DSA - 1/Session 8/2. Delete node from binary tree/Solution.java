7033fce0fb485738209230ee09d4da9b4e7b2f9d
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-11-16 23:48:42 +0530

    Update Solution.java

--- DSA - 1/Session 8/2. Delete node from binary tree/Solution.java
+++ DSA - 1/Session 8/2. Delete node from binary tree/Solution.java
@@ -1,1 +1,65 @@
+import java.io.*;
+import java.util.*;
+import crio.ds.Tree.TreeNode;
 
+/*
+Definition of TreeNode
+class TreeNode {
+    public long val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode (long x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+
+Use new TreeNode(data) to create new Node
+*/
+class Solution {
+
+    public TreeNode binaryTreeDeletion(TreeNode root) {
+        
+        if(root == null){
+            return null;
+        }
+
+        if(root.left == null && root.right == null){
+            root = null;
+            return null;
+        }
+
+        root.left = binaryTreeDeletion(root.left);
+        root.right = binaryTreeDeletion(root.right);
+        return root;
+    }
+
+}
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output & extend it to test cases
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving.
+     a. Obvious logic, tests ability to convert logic to code
+     b. Figuring out logic
+     c. Knowledge of specific domain or concepts
+     d. Knowledge of specific algorithm
+     e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one
+3. Get to a point where you can explain your approach to a 10 year old
+4. Take a stab at the high-level logic & write it down.
+5. Try to offload processing to functions & keeping your main code small.
+Milestone 3: Code by expanding your pseudocode
+1. Make sure you name the variables, functions clearly.
+2. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+3. Use libraries as much as possible
+Milestone 4: Prove to the interviewer that your code works with unit tests
+1. Make sure you check boundary conditions
+2. Time & storage complexity
+3. Suggest optimizations if applicable
+*/
