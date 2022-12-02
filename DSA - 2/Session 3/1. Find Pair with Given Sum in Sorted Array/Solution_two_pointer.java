19a7171cc94b384f7c8ab04df07ae43dcd6e83da
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-12-02 21:05:21 +0530

    Create Solution_two_pointer.java

---
+++ DSA - 2/Session 3/1. Find Pair with Given Sum in Sorted Array/Solution_two_pointer.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class TwoSumInSortedArray {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+
+        boolean res = twoSumInSortedArray(n, arr, k);
+
+        String ans = (res) ? "Present" : "Not Present";
+
+        System.out.println(ans);
+
+    }
+
+    static boolean twoSumInSortedArray(int n, int arr[], int k) {
+        
+        int left = 0; 
+        int right= n-1;
+        while(left < right){
+            if(arr[left] + arr[right] == k) return true;
+            else if(arr[left] + arr[right] < k) left++;
+            else if(arr[left] + arr[right] > k) right--;
+        }
+        // HashSet<Integer> hashSet = new HashSet<Integer>();
+        // for(int i=0; i<arr.length;i++){
+            
+        //     if(hashSet.contains(k - arr[i])){
+        //         return true;
+        //     }
+        //     hashSet.add(arr[i]);
+        // }
+        return false;
+    }
+}
+
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output & extend it to test cases
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving.
+     a. Obvious logic, tests ability to convert logic to code
+     b. Figuring out logic
+     c. Knowledge of specific domain or concepts
+     d. Knowledge of specific algorithm
+     e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one
+3. Get to a point where you can explain your approach to a 10 year old
+4. Take a stab at the high-level logic & write it down.
+5. Try to offload processing to functions & keeping your main code small.
+Milestone 3: Code by expanding your pseudocode
+1. Make sure you name the variables, functions clearly.
+2. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+3. Use libraries as much as possible
+Milestone 4: Prove to the interviewer that your code works with unit tests
+1. Make sure you check boundary conditions
+2. Time & storage complexity
+3. Suggest optimizations if applicable
+*/
