bac0aafa9984c7725d31b3b200f043b21b0d460b
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-12-02 23:16:40 +0530

    Create Solution.java

---
+++ DSA - 2/Session 3/5. Find the container that holds the most water/Solution.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+class ContainerWithMostWater {
+
+    // Complete the function implementation below
+    public int maxArea(int[] height) {
+    int n = height.length;
+    int maxarea = 0;
+    int leftIndex = 0; 
+    int rightIndex = n - 1;
+    while (leftIndex < rightIndex) 
+    {
+        int minHeight = Math.min(height[leftIndex], height[rightIndex]);
+        maxarea = Math.max(maxarea, (rightIndex - leftIndex)*minHeight);
+        if(height[leftIndex] < height[rightIndex])
+            leftIndex = leftIndex + 1;
+        else
+            rightIndex = rightIndex - 1;
+        }
+            return maxarea;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int height[] = new int[n];
+        for(int i = 0 ; i < n ; i++) {
+            height[i] = scanner.nextInt();
+        }
+        scanner.close();
+        int result = new ContainerWithMostWater().maxArea(height);
+        System.out.println(result);
+    }
+}
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output & extend it to test cases
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving.
+     a. Obvious logic, tests ability to convert logic to code
+     b. Figuring out logic
+     c. Knowledge of specific domain or concepts
+     d. Knowledge of specific algorithm
+     e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one
+3. Get to a point where you can explain your approach to a 10 year old
+4. Take a stab at the high-level logic & write it down.
+5. Try to offload processing to functions & keeping your main code small.
+Milestone 3: Code by expanding your pseudocode
+1. Make sure you name the variables, functions clearly.
+2. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+3. Use libraries as much as possible
+Milestone 4: Prove to the interviewer that your code works with unit tests
+1. Make sure you check boundary conditions
+2. Time & storage complexity
+3. Suggest optimizations if applicable
+*/
