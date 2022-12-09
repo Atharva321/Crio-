705617baca9b0ee9481446875310ff33276ce10d
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-12-09 20:15:41 +0530

    Update Solution.java

--- DSA - 2/Session 5/3. Find the maximum sum possible out of all subarrays of size k/Solution.java
+++ DSA - 2/Session 5/3. Find the maximum sum possible out of all subarrays of size k/Solution.java
@@ -1,1 +1,64 @@
+import java.util.*;
 
+class MaximumSubarraySumSizeK {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(maximumSubarraySumSizeK(arr, n, k));
+        sc.close();
+    }
+
+    static int maximumSubarraySumSizeK(int[] arr, int n, int k) {
+        int end=0;
+        int start=0;
+        int maxSum = Integer.MIN_VALUE, currSum = 0;
+        while(end<n){
+            if(end-start+1<k){
+                //System.out.println(maxSum);
+                currSum+=arr[end];
+                end++;
+            }
+            else if(end-start+1 ==k){
+                currSum+=arr[end];
+                maxSum = Math.max(currSum, maxSum);
+                
+                currSum = currSum - arr[start];
+                start++;
+                end++;
+            }
+        }
+        return maxSum;
+    }
+}
+
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output & extend it to test cases
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving.
+     a. Obvious logic, tests ability to convert logic to code
+     b. Figuring out logic
+     c. Knowledge of specific domain or concepts
+     d. Knowledge of specific algorithm
+     e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one
+3. Get to a point where you can explain your approach to a 10 year old
+4. Take a stab at the high-level logic & write it down.
+5. Try to offload processing to functions & keeping your main code small.
+Milestone 3: Code by expanding your pseudocode
+1. Make sure you name the variables, functions clearly.
+2. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+3. Use libraries as much as possible
+Milestone 4: Prove to the interviewer that your code works with unit tests
+1. Make sure you check boundary conditions
+2. Time & storage complexity
+3. Suggest optimizations if applicable
+*/
