6dce439c7b4acbd58e711762f55816ff789fe9f6
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-01-13 22:38:59 +0530

    Update Solution.java

--- DSA - 3/Session 6/6. Given a linked list, determine if it has a cycle in it/Solution.java
+++ DSA - 3/Session 6/6. Given a linked list, determine if it has a cycle in it/Solution.java
@@ -1,1 +1,53 @@
-
+import crio.ds.List.*;
+/*
+public class ListNode {
+   public int val;
+   public ListNode next;
+   public ListNode(int x) { val = x; next = null; }
+}
+*/
+public class Solution {
+    public boolean linkedListCycle(ListNode head) {
+      if(head == null || head.next == null) return false;
+      ListNode slow = head;
+      ListNode fast = head;
+      while(fast!=null){
+        if(fast.next == null | fast.next.next == null) return false;
+        fast = fast.next.next;
+        slow = slow.next;
+        if(slow == fast)
+          return true;
+      }
+      return false;
+    }
+}
+/* 
+  Crio Methodology
+  
+  Milestone 1: Understand the problem clearly
+  1. Ask questions & clarify the problem statement clearly.
+  2. *Type down* an example or two to confirm your understanding of the input/output & extend it to test cases
+  
+  Milestone 2: Finalize approach & execution plan
+  1. Understand what type of problem you are solving.
+       a. Obvious logic, tests ability to convert logic to code
+       b. Figuring out logic
+       c. Knowledge of specific domain or concepts
+       d. Knowledge of specific algorithm
+       e. Mapping real world into abstract concepts/data structures
+  2. Brainstorm multiple ways to solve the problem and pick one
+  3. Get to a point where you can explain your approach to a 10 year old
+  4. Take a stab at the high-level logic & *type it down*.
+  5. Try to offload processing to functions & keeping your main code small.
+  
+  Milestone 3: Code by expanding your pseudocode
+  1. Have frequent runs of your code, dont wait for the end
+  2. Make sure you name the variables, functions clearly.
+  3. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+  4. Use libraries as much as possible
+  
+  Milestone 4: Prove to the interviewer that your code works with unit tests
+  1. Make sure you check boundary conditions
+  2. Time & storage complexity
+  3. Suggest optimizations if applicable
+  */
