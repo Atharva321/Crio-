72865bcadc64fed7a4c2e21b8cee887438eb9f6b
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-01-17 23:42:49 +0530

    Update Solution.java

--- DSA - 3/Session 7/1.  Find diameter of Binary Tree/Solution.java
+++ DSA - 3/Session 7/1.  Find diameter of Binary Tree/Solution.java
@@ -20,6 +20,18 @@ Use new TreeNode(data) to create new Node
 public class Solution {
     int maxDiameter = 0;
     
+    
+     private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        max = Math.max(max, left + right);
+        
+        return Math.max(left, right) + 1;
+    }
+    
     public int diameterOfBinaryTree(TreeNode root) {
         
     }
