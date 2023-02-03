4c632482280544abe9fe3f672da9efb4c89fc772
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-02-03 23:10:31 +0530

    Update Solution.java

--- DSA - 1/Java - 1/Java Loops and Arrays/3.  Find the smallest value of array/Solution.java
+++ DSA - 1/Java - 1/Java Loops and Arrays/3.  Find the smallest value of array/Solution.java
@@ -1,1 +1,26 @@
+// Time  : O(n)
+// Space : O(1)
+
+import java.util.*;
+
+public class Solution {
+
+    static int smallestValueOfArray(int[] numbers, String toReturn) {
+        int indexOfSmallValue = -1;
+        for(int i=0; i<numbers.length; i++){
+            if(smallValue > numbers[i]){
+                smallValue = numbers[i];
+                indexOfSmallValue = i;
+            }
+        }
+        return (toReturn == "index") ? l : numbers[lowest];;
+    }
+
+    public static void main(String args[]) {
+        assert (1 == smallestValueOfArray(new int[] { 4, 2, 3, 1, 5 }, "value")) : "Expect 1 for numbers = [4, 2, 3, 1, 5] and toReturn = \"index\"";
+        assert (3 == smallestValueOfArray(new int[] { 4, 2, 3, 1, 5 }, "index")) : "Expect 3 for numbers = [4, 2, 3, 1, 5] and toReturn = \"index\"";
+        System.out.println("All test cases in main function passed");
+
+    }
+}
 
