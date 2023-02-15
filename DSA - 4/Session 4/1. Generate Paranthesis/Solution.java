9348656b2fb1544a649cc0cf52e9b8fddf5cb8fa
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-02-15 22:42:43 +0530

    Update Solution.java

--- DSA - 4/Session 4/1. Generate Paranthesis/Solution.java
+++ DSA - 4/Session 4/1. Generate Paranthesis/Solution.java
@@ -1,1 +1,35 @@
+// Time  : Very Complex
+// Space : O(2^n)
+import java.util.*;
 
+public class BalancedBraces {
+    ArrayList<String> ans = new ArrayList<>();
+    ArrayList<String> balancedBraces(int n) {
+        parenthesesGenerator(0, 0, "", n);
+        return ans;
+    }
+    
+    public void parenthesesGenerator(int open, int close, String  curr, int n){
+        if(curr.length() == n*2){
+            ans.add(curr);
+            return;
+        }
+        if(open < n){
+            parenthesesGenerator(open+1, close, curr+'(', n);
+        }
+        
+        if(open > close) { 
+            parenthesesGenerator(open, close+1, curr+')', n);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<String> answer = new BalancedBraces().balancedBraces(n);
+        for(int i = 0 ; i < answer.size() ; i++) {
+            System.out.println(answer.get(i));
+        }
+    }
+}
