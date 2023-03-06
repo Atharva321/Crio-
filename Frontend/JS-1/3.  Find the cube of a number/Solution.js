4458bd7fc167554723830c1b6ad74c2d1a98f5db
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-06 22:50:41 +0530

    Update Solution.js

--- Frontend/JS-1/3.  Find the cube of a number/Solution.js
+++ Frontend/JS-1/3.  Find the cube of a number/Solution.js
@@ -1,1 +1,18 @@
+/**
+ * @param {number} a
+ * @return {number}
+ */
+function findCube(a) {
+    // You only need to implement this function.
+    return a*a*a;
+}
+
+
+if (findCube(2) !== 8)
+    console.log("Test fails: Expected 8 for input a = 2");
+else
+    console.log("Sample test case for input a = 2 passed!");
+
+
+module.exports = findCube;
 
