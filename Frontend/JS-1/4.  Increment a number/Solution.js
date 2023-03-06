b63c625a90c265c8dcb1edcb8650f2597bf23fdb
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-06 22:52:57 +0530

    Update Solution.js

--- Frontend/JS-1/4.  Increment a number/Solution.js
+++ Frontend/JS-1/4.  Increment a number/Solution.js
@@ -1,1 +1,19 @@
+// Time  : O(1)
+// Space : O(1)
 
+/**
+ * @param {number} a
+ * @return {number}
+ */
+
+function incrementNumber(a) {
+    // You only need to implement this function.
+    return ++a;
+}
+
+if (incrementNumber(2) !== 3)
+    console.log("Test fails: Expected 3 for input a = 2");
+else
+    console.log("Sample test case for input a = 2 passed!");
+
+module.exports = incrementNumber;
