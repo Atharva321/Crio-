66327d7915193b29f8871c5fbabe0c6a4a83df8f
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-07 23:14:11 +0530

    Update Solution.java

--- DSA - 5/Session 1/2. Find number of ways to climb N steps if you can take 1 or 2 steps at a time/Solution.java
+++ DSA - 5/Session 1/2. Find number of ways to climb N steps if you can take 1 or 2 steps at a time/Solution.java
@@ -1,1 +1,56 @@
+import java.io.*;
+import java.util.*;
 
+public class ClimbingStairs {
+
+    public int climbingStairs(int n) {
+        if(n==1) return 1;
+        int first = 1;
+        int second = 2;
+        for(int i=3; i<=n; i++){
+            int third = first+second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStairs = scanner.nextInt();
+        scanner.close();
+        int result = new ClimbingStairs().climbingStairs(numberOfStairs);
+        System.out.print(result);
+    }
+}
+
+/* 
+  Crio Methodology
+  
+  Milestone 1: Understand the problem clearly
+  1. Ask questions & clarify the problem statement clearly.
+  2. *Type down* an example or two to confirm your understanding of the input/output & extend it to test cases
+  
+  Milestone 2: Finalize approach & execution plan
+  1. Understand what type of problem you are solving.
+       a. Obvious logic, tests ability to convert logic to code
+       b. Figuring out logic
+       c. Knowledge of specific domain or concepts
+       d. Knowledge of specific algorithm
+       e. Mapping real world into abstract concepts/data structures
+  2. Brainstorm multiple ways to solve the problem and pick one
+  3. Get to a point where you can explain your approach to a 10 year old
+  4. Take a stab at the high-level logic & *type it down*.
+  5. Try to offload processing to functions & keeping your main code small.
+  
+  Milestone 3: Code by expanding your pseudocode
+  1. Have frequent runs of your code, dont wait for the end
+  2. Make sure you name the variables, functions clearly.
+  3. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+  4. Use libraries as much as possible
+  
+  Milestone 4: Prove to the interviewer that your code works with unit tests
+  1. Make sure you check boundary conditions
+  2. Time & storage complexity
+  3. Suggest optimizations if applicable
+  */
