0e1d5d3913faca4e89868c72171a70ac77ed292b
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-08 21:51:00 +0530

    Update Solution.js

--- Frontend/JS-1/Session 2/1. Multiply two decimal numbers Post Session [JS] Javascript Operators and Conditionals 2. Check if two numbers are equal/Solution.js
+++ Frontend/JS-1/Session 2/1. Multiply two decimal numbers Post Session [JS] Javascript Operators and Conditionals 2. Check if two numbers are equal/Solution.js
@@ -1,1 +1,21 @@
+// Time  : O(1)
+// Space : O(1)
 
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {boolean}
+ */
+
+function isEqual(a, b) {
+    // You only need to implement this function.
+    return a===b;
+}
+
+if (isEqual(3,3) !== true)
+    console.log("Test fails: Expected true for input a = 3 and b = 3");
+else
+    console.log("Sample test case for input a = 3 and b = 3 passed!");
+
+
+module.exports = isEqual;
