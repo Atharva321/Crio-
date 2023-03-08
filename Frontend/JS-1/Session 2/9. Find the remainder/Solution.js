99f0e0efade00bb5dda83f8c15931ef01d0740f2
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-08 22:14:13 +0530

    Create Solution.js

---
+++ Frontend/JS-1/Session 2/9. Find the remainder/Solution.js
@@ -0,0 +1,19 @@
+// Time  : O(1)
+// Space : O(1)
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+function remainderOfDivision(a, b) {
+    // You only need to implement this function.
+    return a%b;
+}
+
+if (remainderOfDivision(10,3) !== 1)
+    console.log("Test fails: Expected 1 for input a = 10 and b = 3");
+else
+    console.log("Sample test case for input a = 10 and b = 3 passed!");
+    
+module.exports = remainderOfDivision;
