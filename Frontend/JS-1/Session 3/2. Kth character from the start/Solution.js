83622ea03b259607b6762b86aed84eed9528d986
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-10 23:06:50 +0530

    Create

---
+++ Frontend/JS-1/Session 3/2. Kth character from the start/Solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} str
+ * @param {number} k
+ * @return {string}
+ */
+
+
+function kthCharFromStart(str, k){
+  // You only need to implement this function.
+  return str[k-1];
+}
+
+if (kthCharFromStart("abcdefghi", 2) !== 'b')
+  console.log("Test fails: Expected 'b' for input str = 'abcdefghi' and k = 2");
+else
+  console.log("Sample test case for input str = 'abcdefghi' and k = 2 passed!");
+
+module.exports = kthCharFromStart
