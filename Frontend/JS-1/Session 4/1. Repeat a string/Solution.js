99bc02c4cbb75121d6f918415fe3b44384ec84d2
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-11 23:47:57 +0530

    Update Solution.js

--- Frontend/JS-1/Session 4/1. Repeat a string/Solution.js
+++ Frontend/JS-1/Session 4/1. Repeat a string/Solution.js
@@ -1,1 +1,27 @@
+// Time  : O(m*n)
+// Space : O(m*n)
 
+/**
+ * @param {string} str
+ * @param {number} k
+ * @return {string}
+ */
+
+
+function repeatString(str, k){
+  // You only need to implement this function.
+  repeatedString = "";
+  while(k>0){
+    repeatedString+=str;
+    k--;
+  }
+  // console.log(repeatedString);
+  return repeatedString;
+}
+
+if (repeatString("!", 3) !== "!!!")
+  console.log("Test fails: Expected '!!!' for input str = '!' and k = 3");
+else
+  console.log("Sample test case for input str = '!' and k = 3 passed!");
+
+module.exports = repeatString
