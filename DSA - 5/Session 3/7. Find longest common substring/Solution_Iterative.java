774f504368827bb6eac03bdaa13231669a259e21
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-11 23:44:19 +0530

    Update Solution_Iterative.java

--- DSA - 5/Session 3/7. Find longest common substring/Solution_Iterative.java
+++ DSA - 5/Session 3/7. Find longest common substring/Solution_Iterative.java
@@ -1,1 +1,72 @@
+import java.util.*;
 
+class LongestCommonSubstring{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            String s1 = sc.next();
+            String s2 = sc.next();
+            System.out.println(longestCommonSubstring(s1,s2));
+            t--;
+        }
+        sc.close();
+    }
+
+    static int longestCommonSubstring(String s1, String s2){
+        int [][] dp = new int [s1.length()+1][s2.length()+1];
+        for(int i=0; i<=s1.length(); i++){
+            dp[i][0] = 0;
+        }
+        for(int i=0; i<=s2.length(); i++){
+            dp[0][i] = 0;
+        }
+
+        int subString = 0;
+
+        for(int i=1; i<=s1.length(); i++){
+            for(int j=1; j<=s2.length(); j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    subString = Math.max(subString, dp[i][j]);
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return subString;
+    }
+}
+
+/* 
+  Crio Methodology
+  
+  Milestone 1: Understand the problem clearly
+  1. Ask questions & clarify the problem statement clearly.
+  2. *Type down* an example or two to confirm your understanding of the input/output & extend it to test cases
+  
+  Milestone 2: Finalize approach & execution plan
+  1. Understand what type of problem you are solving.
+       a. Obvious logic, tests ability to convert logic to code
+       b. Figuring out logic
+       c. Knowledge of specific domain or concepts
+       d. Knowledge of specific algorithm
+       e. Mapping real world into abstract concepts/data structures
+  2. Brainstorm multiple ways to solve the problem and pick one
+  3. Get to a point where you can explain your approach to a 10 year old
+  4. Take a stab at the high-level logic & *type it down*.
+  5. Try to offload processing to functions & keeping your main code small.
+  
+  Milestone 3: Code by expanding your pseudocode
+  1. Have frequent runs of your code, dont wait for the end
+  2. Make sure you name the variables, functions clearly.
+  3. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+  4. Use libraries as much as possible
+  
+  Milestone 4: Prove to the interviewer that your code works with unit tests
+  1. Make sure you check boundary conditions
+  2. Time & storage complexity
+  3. Suggest optimizations if applicable
+  */
