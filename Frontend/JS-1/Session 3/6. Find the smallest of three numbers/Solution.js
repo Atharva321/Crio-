9940ed51158b60f7b3371493f7f8c47aab56072a
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-12 23:41:14 +0530

    Update Solution.js

--- Frontend/JS-1/Session 3/6. Find the smallest of three numbers/Solution.js
+++ Frontend/JS-1/Session 3/6. Find the smallest of three numbers/Solution.js
@@ -1,1 +1,25 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+function findSmallest(a, b, c) {
+      // You only need to implement this function.
+      if(a > b){
+          if(b > c) return c;
+          return b;
+      }
+      else if(a > c){
+          return c;
+      }
+      return a;
+}
+
+if (findSmallest(1,2,3) !== 1)
+    console.log("Test fails: Expected 1 for input a = 1, b = 2 and c = 3");
+else
+    console.log("Sample test case for input a = 1, b = 2 and c = 3 passed!");
+
+module.exports = findSmallest;
 
