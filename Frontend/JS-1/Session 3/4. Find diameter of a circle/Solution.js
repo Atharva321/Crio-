92a4b72226518c2a5647ac0eea6823b65c7b9dd1
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-12 23:36:52 +0530

    Update Solution.js

--- Frontend/JS-1/Session 3/4. Find diameter of a circle/Solution.js
+++ Frontend/JS-1/Session 3/4. Find diameter of a circle/Solution.js
@@ -1,1 +1,18 @@
+/**
+ * @param {number} r
+ * @return {string}
+ */
+
+
+function circleValues(r){
+  // You only need to implement this function.
+  return "A circle of radius " + r + " has a diameter " + 2*r;
+}
+
+if (circleValues(2) !== "A circle of radius 2 has a diameter 4")
+  console.log("Test fails: Expected 'A circle of radius 2 has a diameter 4' for input r = 2");
+else
+  console.log("Sample test case for input r = 2 passed!");
+
+module.exports = circleValues
 
