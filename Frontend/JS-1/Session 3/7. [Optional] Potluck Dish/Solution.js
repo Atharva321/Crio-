d48a769abee6c240f5f2dfccc981caf9a762cc21
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-12 23:42:35 +0530

    Update Solution.js

--- Frontend/JS-1/Session 3/7. [Optional] Potluck Dish/Solution.js
+++ Frontend/JS-1/Session 3/7. [Optional] Potluck Dish/Solution.js
@@ -1,1 +1,19 @@
+/**
+ * @param {string} name
+ * @param {string} dish
+ * @return {boolean}
+ */
+
+
+function potluckDish(name, dish) {
+  // You only need to implement this function.
+  return name[0] + name[name.length - 1] === dish[0] + dish[dish.length - 1];
+}
+
+if (potluckDish("Jhony", "Jelly") !== true)
+  console.log("Test fails: Expected true for input name = Jhony and dish = Jelly");
+else
+  console.log("Sample test case for input name = Jhony and dish = Jelly passed!");
+
+module.exports = potluckDish
 
