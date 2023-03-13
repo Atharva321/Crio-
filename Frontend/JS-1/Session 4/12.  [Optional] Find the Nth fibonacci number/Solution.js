6214e8a91cd000b4b268bbf1148c66e7e0801931
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-13 23:40:51 +0530

    Update Solution.js

--- Frontend/JS-1/Session 4/12.  [Optional] Find the Nth fibonacci number/Solution.js
+++ Frontend/JS-1/Session 4/12.  [Optional] Find the Nth fibonacci number/Solution.js
@@ -1,1 +1,31 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+
+function findFibonacci(n) {
+  // You only need to implement this function.
+  let first = 0;
+  let second = 1;
+
+  if(n==0) return 0;
+  if(n==1) return 1;
+  if(n==2) return second;
+
+  for(let i=3; i<=n+1; i++){
+    let temp = first + second;
+    first = second;
+    second = temp;
+  }
+  // console.log(second);
+  return second;
+}
+
+if (findFibonacci(5) !== 5)
+  console.log("Test fails: Expected 5 for input n = 5");
+else
+  console.log("Sample test case for input n = 5 passed!");
+
+module.exports = findFibonacci
 
