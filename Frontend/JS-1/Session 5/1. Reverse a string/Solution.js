c8711aa2cf04fcaa4fd67fe624454fc896bbc0a1
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-15 22:54:04 +0530

    Update Solution.js

--- Frontend/JS-1/Session 5/1. Reverse a string/Solution.js
+++ Frontend/JS-1/Session 5/1. Reverse a string/Solution.js
@@ -1,1 +1,27 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
 
+
+function reverseString(str){
+  // You only need to implement this function.
+  arr = str.split('');
+
+  start = 0;
+  end = arr.length-1;
+
+  while(start < end){
+    temp = arr[start];
+    arr[start++] = arr[end];
+    arr[end--] = temp;  
+  }
+  return arr.join('');
+}
+
+if (reverseString("abcdefghi") !== "ihgfedcba")
+  console.log("Test fails: Expected 'ihgfedcba' for input str = 'abcdefghi'");
+else
+  console.log("Sample test case for input str = 'abcdefghi' passed!");
+
+module.exports = reverseString
