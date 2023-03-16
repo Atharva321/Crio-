2a68d46768d558bf8f8eb5e000774873357a9f94
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-16 23:28:54 +0530

    Update Solution.js

--- Frontend/JS-1/Session 5/5. Longest word in a string/Solution.js
+++ Frontend/JS-1/Session 5/5. Longest word in a string/Solution.js
@@ -1,1 +1,25 @@
+/**
+ * @param {string} str
+ * @return {number}
+ */
+
+
+function findLongestWordLength(str){
+  // You only need to implement this function.
+  arr = str.split(' ');
+  // console.log(arr);
+  length = 0;
+
+  for(i=0; i<arr.length; i++){
+    length = length < arr[i].length ? arr[i].length : length;
+  }
+  return length;
+}
+
+if (findLongestWordLength("I live in India") !== 5)
+  console.log("Test fails: Expected 5 for input str = 'I live in India'");
+else
+  console.log("Sample test case for input str = 'I live in India' passed!");
+
+module.exports = findLongestWordLength
 
