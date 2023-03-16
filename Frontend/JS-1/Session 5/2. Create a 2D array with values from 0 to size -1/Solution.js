8c9d97961b65dd574f1fc768afb41fc99a9e4b2c
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-16 23:23:33 +0530

    Update Solution.js

--- Frontend/JS-1/Session 5/2. Create a 2D array with values from 0 to size -1/Solution.js
+++ Frontend/JS-1/Session 5/2. Create a 2D array with values from 0 to size -1/Solution.js
@@ -1,1 +1,29 @@
+/**
+ * @param {number} size
+ * @return {number[][]}
+ */
 
+
+function createMatrix(size) {
+  // You only need to implement this function.
+  let arr = [];
+let rows = size;
+let columns = size;
+
+let value = 0;
+// creating two-dimensional array
+for (let i = 0; i < rows; i++) {
+  arr[i] = [];
+  for (let j = 0; j < columns; j++) {
+    arr[i][j] = j;
+  }
+}
+return arr;
+}
+
+if (JSON.stringify(createMatrix(2)) !== JSON.stringify([[0, 1], [0, 1]]))
+  console.log("Test fails: Expected [ [ 0, 1 ], [ 0, 1 ] ] for input size = 2");
+else
+  console.log("Sample test case for input size = 2 passed!");
+
+module.exports = createMatrix
