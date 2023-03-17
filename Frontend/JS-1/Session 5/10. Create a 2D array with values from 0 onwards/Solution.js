80d64cc8addaa9a752996830b33a198ac2cbce01
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-17 23:32:20 +0530

    Update Solution.js

--- Frontend/JS-1/Session 5/10. Create a 2D array with values from 0 onwards/Solution.js
+++ Frontend/JS-1/Session 5/10. Create a 2D array with values from 0 onwards/Solution.js
@@ -1,1 +1,32 @@
+// Time  : O(n^2)
+// Space : O(1)
 
+/**
+ * @param {number} size
+ * @return {number[][]}
+ */
+
+
+function createMatrixII(size){
+  // You only need to implement this function.
+  let arr = [];
+let rows = size;
+let columns = size;
+
+let value = 0;
+// creating two-dimensional array
+for (let i = 0; i < rows; i++) {
+  arr[i] = [];
+  for (let j = 0; j < columns; j++) {
+    arr[i][j] = value++;
+  }
+}
+return arr;
+}
+
+if (JSON.stringify(createMatrixII(2)) !== JSON.stringify([ [ 0, 1 ], [ 2, 3 ] ]))
+  console.log("Test fails: Expected [ [ 0, 1 ], [ 2, 3 ] ] for input size = 2");
+else
+  console.log("Sample test case for input size = 2 passed!");
+
+module.exports = createMatrixII
