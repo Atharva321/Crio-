62e4e5d8876637458fba077fe5986bbafbdadc7d
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-17 23:29:35 +0530

    Update Solution.js

--- Frontend/JS-1/Session 5/9. Find the total number of student/Solution.js
+++ Frontend/JS-1/Session 5/9. Find the total number of student/Solution.js
@@ -1,1 +1,24 @@
+/**
+ * @param {number[][]} arr
+ * @return {number}
+ */
+
+
+function matrixSum(arr) {
+  // You only need to implement this function.
+  sum = 0;
+  for(i=0; i<arr.length; i++){
+    for(j=0; j<arr[0].length; j++){
+      sum += arr[i][j];
+    }
+  }
+  return sum;
+}
+
+if (matrixSum([[5, 1], [2, 4]]) != 12)
+  console.log("Test fails: Expected 12 for input arr = [[5, 1], [2, 4]]");
+else
+  console.log("Sample test case for input arr = [[5, 1], [2, 4]] passed!");
+
+module.exports = matrixSum
 
