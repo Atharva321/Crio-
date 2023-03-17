5272b2748363960972597fb010bb67df5232f59b
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-17 23:24:25 +0530

    Update Solution.js

--- Frontend/JS-1/Session 5/6. Palindrome or not /Solution.js
+++ Frontend/JS-1/Session 5/6. Palindrome or not /Solution.js
@@ -1,1 +1,23 @@
+/**
+ * @param {string} str
+ * @return {boolean}
+ */
+
+
+function checkPalindrome(str){
+  // You only need to implement this function.
+  start = 0;
+  end = str.length - 1;
+  while(start < end){
+    if(str[start++] != str[end--]) return false;
+  }
+  return true;
+}
+
+if (checkPalindrome("madam") !== true)
+  console.log("Test fails: Expected true for input str = 'madam'");
+else
+  console.log("Sample test case for input str = 'madam' passed!");
+
+module.exports = checkPalindrome
 
