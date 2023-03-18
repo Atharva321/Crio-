a263066a71227c984a9245a5712dabdc00eb61e3
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-18 23:16:35 +0530

    Update Solution.java

--- DSA - 1/Java - 1/Session 1/4. Check if two numbers are equal/Solution.java
+++ DSA - 1/Java - 1/Session 1/4. Check if two numbers are equal/Solution.java
@@ -1,1 +1,14 @@
+import java.util.*;
 
+public class Solution {
+
+    public static boolean isEqual(int a, int b) {
+        if(a==b) return true;
+        return false;
+    }
+    
+    public static void main(String args[]) {
+        assert (isEqual(100, 100) == true) : "Expect true for a = 100, b = 100";
+        System.out.println("All test cases in main function passed");
+    }
+}
