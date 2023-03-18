621da151fb6484b44e67bfe5995be5851df73347
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-18 23:09:38 +0530

    Update Solution.java

--- DSA - 1/Java - 1/Session 1/1. Find the sum of two numbers/Solution.java
+++ DSA - 1/Java - 1/Session 1/1. Find the sum of two numbers/Solution.java
@@ -1,1 +1,14 @@
+import java.util.*;
 
+public class Solution {
+
+    public static int sumOfTwoNumbers(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String args[]) {
+        assert (sumOfTwoNumbers(100, 899) == 999) : "Expect 666 for a = 100, b = 566";
+        System.out.println("All test cases in main function passed");
+
+    }
+}
