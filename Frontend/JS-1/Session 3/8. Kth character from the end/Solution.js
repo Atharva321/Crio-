6fcc4533844c864bda4509034fa2a4bf7590a991
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-19 23:44:06 +0530

    Update Solution.js

--- Frontend/JS-1/Session 3/8. Kth character from the end/Solution.js
+++ Frontend/JS-1/Session 3/8. Kth character from the end/Solution.js
@@ -1,1 +1,18 @@
+/**
+ * @param {string} str
+ * @param {number} k
+ * @return {string}
+ */
 
+
+function kthCharFromEnd(str, k){
+  // You only need to implement this function.
+  return str[str.length - k];
+}
+
+if (kthCharFromEnd("abcdefghi", 2) !== 'h')
+  console.log("Test fails: Expected 'h' for input str = 'abcdefghi' and k = 2");
+else
+  console.log("Sample test case for input str = 'abcdefghi' and k = 2 passed!");
+
+module.exports = kthCharFromEnd
