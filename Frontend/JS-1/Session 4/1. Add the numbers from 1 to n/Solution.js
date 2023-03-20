4aa18c91097cc162a5a8fe4df600a2a0b94d8f89
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-20 23:19:52 +0530

    Update Solution.js

--- Frontend/JS-1/Session 4/1. Add the numbers from 1 to n/Solution.js
+++ Frontend/JS-1/Session 4/1. Add the numbers from 1 to n/Solution.js
@@ -1,1 +1,25 @@
+// Time  : O(n)
+// Space : O(1)
 
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+
+function findSumOfFirstN(n){
+  // You only need to implement this function.
+  let sum = 0;
+  for(i=1; i<=n; i++){
+    sum+=i;
+  }
+  return sum;
+}
+
+if (findSumOfFirstN(3) !== 6)
+  console.log("Test fails: Expected 6 for input n = 3");
+else
+  console.log("Sample test case for input n = 3 passed!");
+
+
+module.exports = findSumOfFirstN
