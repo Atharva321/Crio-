c06a0908c1ed54b03646e45559906fde5943b829
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-24 22:38:23 +0530

    Update Solution.js

--- Frontend/JS-1/Session 8/1. [Debug] Fix the declaration/Solution.js
+++ Frontend/JS-1/Session 8/1. [Debug] Fix the declaration/Solution.js
@@ -1,1 +1,15 @@
+// Time  : O(1)
+// Space : O(1)
+/**
+ * @return {void}
+ */
 
+
+function doSomething() {
+  console.log(x);
+  var x = 1;
+}
+
+console.log("Do not change the order of statements. Check whether you can use a different keyword to declare a variable and try debugging.");
+
+module.exports = doSomething;
