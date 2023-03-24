0db8bd99b6b8912ca0e2f338d788107599aed437
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-24 22:43:05 +0530

    Create Solution.js

---
+++ Frontend/JS-1/Session 6/3. Modify nested object/Solution.js
@@ -0,0 +1,25 @@
+// Time  : O(1)
+// Space : O(1)
+
+/**
+ * @param {object}obj
+ * @param {number}num
+ * @return {object}
+ */
+
+
+function modifyNestedObject(obj, num) {
+  // You only need to implement this function.
+  obj.address.streetNumber = num;
+  return obj; 
+}
+
+
+if (modifyNestedObject({ address: { streetNumber: 5 } }, 8).address.streetNumber !== 8)
+  console.log("Test fails: Expected { address: { streetNumber: 8}} for input obj = { address: { streetNumber: 5 }}, and num = 8");
+else
+  console.log("Sample test case for input obj = { address: { streetNumber: 5 }}, and num = 8 passed!");
+
+
+module.exports = modifyNestedObject
+
