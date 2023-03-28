8f8ff37aae2bf96a3c83ba12a493ee2885836e15
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-28 23:11:05 +0530

    Update Solution.js

--- Frontend/JS-1/Session 5/4. [Debug] Accessing object 2/Solution.js
+++ Frontend/JS-1/Session 5/4. [Debug] Accessing object 2/Solution.js
@@ -1,1 +1,11 @@
+/**
+ * @return {string}
+ */
+function getFirstItem() {
+  const shoppingCart = {"One": "Apple", "Two": "Orange"};
+  return shoppingCart.One;
+}
 
+console.log("You can use console statements to check how to access properties of an object and try debugging.");
+
+module.exports = getFirstItem;
