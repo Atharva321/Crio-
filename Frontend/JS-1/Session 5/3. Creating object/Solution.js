70cdf8d1d8123b82bb247c34da5b1a4c05009639
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-28 23:09:04 +0530

    Update Solution.js

--- Frontend/JS-1/Session 5/3. Creating object/Solution.js
+++ Frontend/JS-1/Session 5/3. Creating object/Solution.js
@@ -1,1 +1,23 @@
+// Time  : O(1)
 
+/**
+ * @param {string}firstName
+ * @param {string}middleName
+ * @param {string}lastName
+ * @return {object}
+ */
+
+
+function getFullName(firstName,middleName,lastName){
+  // You only need to implement this function.
+  var fullName =  firstName + " " + middleName + " " + lastName;
+  return fullName;
+ }
+
+if ((getFullName('Alex','P','John')).fullName !== 'Alex P John')
+  console.log("Test fails: Expected { fullName: 'Alice P John' } for input firstName ='Alex', middleName = 'P', and lastName = 'John'");
+else
+  console.log("Sample test case for input firstName ='Alex', middleName = 'P', and lastName = 'John' passed!");
+
+
+module.exports = getFullName
