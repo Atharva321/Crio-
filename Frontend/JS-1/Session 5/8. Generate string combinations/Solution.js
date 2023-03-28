1c9c9ff9adf4001414e637f55339b2025d6e085e
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-03-28 23:14:23 +0530

    Update Solution.js

--- Frontend/JS-1/Session 5/8. Generate string combinations/Solution.js
+++ Frontend/JS-1/Session 5/8. Generate string combinations/Solution.js
@@ -1,3 +1,6 @@
+// Time  : O(n)
+// Space : O(n^2)
+
 /**
  * @param {string[]} arr1
  * @param {string[]} arr2
@@ -6,9 +9,15 @@
 
 
 function generateCombinations(arr1, arr2) {
-  // You only need to implement this function.
-  Sy
-  return arr1*arr2;
+  // You only need to implement this function
+  let arr3 = new Array(arr1.length*arr2.length);
+  let k=0;
+  for(let i=0;i<arr1.length; i++){
+    for(let j=0;j<arr2.length; j++){
+      arr3[k++] = arr1[i]+arr2[j];
+    }
+  }
+  return arr3;
 }
 
 if (JSON.stringify(generateCombinations(['a', 'c', 'e'], ['b', 'd', 'f'])) !== JSON.stringify( ['ab', 'ad', 'af', 'cb', 'cd', 'cf', 'eb', 'ed', 'ef']))
