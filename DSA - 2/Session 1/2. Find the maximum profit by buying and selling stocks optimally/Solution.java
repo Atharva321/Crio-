e108876b95fcb6a067ebce2e259923605e072932
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-02 19:24:54 +0530

    Update Solution.java

--- DSA - 2/Session 1/2. Find the maximum profit by buying and selling stocks optimally/Solution.java
+++ DSA - 2/Session 1/2. Find the maximum profit by buying and selling stocks optimally/Solution.java
@@ -3,10 +3,7 @@ import java.util.*;
 
 public class BestTimeToBuyAndSellStocks {
     // Implement your solution Here
-    /*
-    Time : O(n)
-    Space: O(1)
-    */
+    
     public int maxProfit(int[] prices) {
          int profit = 0;
          for(int i=0; i<prices.length-1; i++){
