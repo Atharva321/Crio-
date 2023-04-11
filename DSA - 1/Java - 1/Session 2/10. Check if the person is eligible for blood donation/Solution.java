cfccb114ad1e65a2d6c796448e70049c7ab60b49
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-11 22:52:06 +0530

    Update Solution.java

--- DSA - 1/Java - 1/Session 2/10. Check if the person is eligible for blood donation/Solution.java
+++ DSA - 1/Java - 1/Session 2/10. Check if the person is eligible for blood donation/Solution.java
@@ -1,1 +1,19 @@
+// Time  : O(1)
+// Space : O(1)
 
+import java.util.*;
+
+public class Solution {
+
+  public static String checkBloodDonationEligiblity(int age, int weight) {
+    if(age < 18) return "UnderAge";
+    else if(weight <= 50) return "NotEligible";
+    return "Eligible";
+  }
+
+  public static void main(String args[]) {
+        assert (checkBloodDonationEligiblity(25, 55) == "Eligible") : "Expect Eligible for a = 25, b = 55";
+        System.out.println("All test cases in main function passed");
+
+    }
+}
