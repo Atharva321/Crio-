929b8135b300ce2ac22aed71010db302994d88bf
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-11 22:48:32 +0530

    Update Solution.java

--- DSA - 1/Java - 1/Session 2/8. Find the number of centuries for given year/Solution.java
+++ DSA - 1/Java - 1/Session 2/8. Find the number of centuries for given year/Solution.java
@@ -1,1 +1,17 @@
+import java.util.*;
 
+public class Solution {
+
+  public static int findCenturyFromYear(int a) {
+    if(a%100!=0){
+      return 1+ a/100;
+    }
+    else return a/100;
+  }
+
+  public static void main(String args[]) {
+        assert (findCenturyFromYear(1705) == 18) : "Expect 18 for a = 1705";
+        System.out.println("All test cases in main function passed");
+
+    }
+}
