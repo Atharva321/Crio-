8c4a121f1eeb08752b4413ea6bc1e43ef4fc7196
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-11 22:50:40 +0530

    Update Solution.java

--- DSA - 1/Java - 1/Session 2/9. Find the smallest of three numbers/Solution.java
+++ DSA - 1/Java - 1/Session 2/9. Find the smallest of three numbers/Solution.java
@@ -1,1 +1,26 @@
+// Time  : O(1)
+// Space : O(1)
+
+import java.util.*;
+
+public class Solution {
+
+  public static int findSmallest(int a, int b, int c) {
+     if(a < b) {
+      if(a < c)
+        return a;
+      return c;
+    } else {
+      if(b < c)
+        return b;
+      return c;
+    }
+  }
+
+  public static void main(String args[]) {
+        assert (findSmallest(1, 9, 78) == 1) : "Expect 1 for a = 1, b = 9, c = 78";
+        System.out.println("All test cases in main function passed");
+
+    }
+}
 
