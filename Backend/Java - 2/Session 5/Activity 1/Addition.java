6a2e4b37c4db14ca43210e70a56ccb323e82bf65
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-12 20:54:29 +0530

    Update Addition.java

--- Backend/Java - 2/Session 5/Activity 1/Addition.java
+++ Backend/Java - 2/Session 5/Activity 1/Addition.java
@@ -1,1 +1,18 @@
+class SimpleAddition {
+    int add(int a, int b){
+        return a+b;
+    }
 
+    int add(int a, int b, int c){
+        return a+b+c;
+    }
+}
+
+public class Addition {
+    public static void main(String[] args){
+        SimpleAddition obj = new SimpleAddition();
+        System.out.println(obj.add(18 ,27));
+        System.out.println(obj.add(9 ,18, 18));
+        System.out.println("Code is Working....");
+    }
+}
