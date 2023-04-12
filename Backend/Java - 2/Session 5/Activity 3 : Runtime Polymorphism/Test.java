76faf474eb930267638b06d6c1df42951eacf397
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-12 21:36:50 +0530

    Update Test.java

--- Backend/Java - 2/Session 5/Activity 3 : Runtime Polymorphism/Test.java
+++ Backend/Java - 2/Session 5/Activity 3 : Runtime Polymorphism/Test.java
@@ -1,1 +1,30 @@
+class Bank {
+    int getRateOfInterest(){
+        return 5;
+    }
+}
 
+class SBI extends Bank{
+    int getRateOfInterest(){
+        System.out.println("SBI Interest: 8%");
+        return 8;
+    }
+}
+
+class ICICI extends Bank{
+    int getRateOfInterest(){
+        System.out.println("SBI Interest: 7%");
+        return 7;
+    }
+}
+
+
+public class Test {
+    public static void main(String[] args){
+        SBI sbi = new SBI();
+        System.out.println(sbi.getRateOfInterest());
+        ICICI icici = new ICICI();
+        System.out.println(icici.getRateOfInterest());
+        System.out.println("Code is Working....");
+    }
+}
