634e0ba59bdce8f301d2723114efce5a61929421
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-12 21:08:33 +0530

    Update Addition_2.java

--- Backend/Java - 2/Session 5/Activity 1/Addition_2.java
+++ Backend/Java - 2/Session 5/Activity 1/Addition_2.java
@@ -1,26 +1,32 @@
 //Type 2 and Type 3 Changing  Data Type of Argument and Order of Arguments
 
 class ComplexAddition {
+    
+    // Type 1
     int add(int a, int b){
         System.out.println("Method of a: int, b: int is called.");
         return a+b;
     }
-
+    
+    // Type 2
     double add(double a, double b){
         System.out.println("Method of a: double, b: double is called.");
         return a+b;
     }
-
+    
+    // Type 3
     int add(double a, int b){
         System.out.println("Method of a: double, b: int is called.");
         return (int)a+b;
     }
 
+    // Type 3
     int add(int a, double b){
         System.out.println("Method of a: int, b: double is called.");
         return a+(int)b;
     }
 
+    // Type 1
     int add(int a, int b, int c){
         System.out.println("Method of a: int, b: int, c: int is called.");
         return a+b+c;
