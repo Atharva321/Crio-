844c7725acfc55738146de31598e40cdf58eb8e7
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-13 23:35:09 +0530

    Update Solution.java

--- DSA - 1/Java - 1/Session 4/3. [Debug] Fix the birthday gifts issue/Solution.java
+++ DSA - 1/Java - 1/Session 4/3. [Debug] Fix the birthday gifts issue/Solution.java
@@ -1,1 +1,23 @@
+import java.util.*;
 
+public class Solution {
+
+    static String happyBirthday(int gifts){
+        String[] gifts_response = {"Hardly any", "We need some more gifts", "Woah that's a lot of gifts!", "Happy Birthday Jhandu!!!"};
+    
+        String respond  = gifts <= 5 ? gifts_response[0] : gifts < 23 ? gifts_response[1] : gifts == 23  ? gifts_response[3] : gifts_response[2];
+    
+        return respond;
+        
+        
+    }
+
+    public static void main(String args[]) {
+        int gifts = 99;
+        assert happyBirthday(gifts).equals("Woah that's a lot of gifts!") ;
+        gifts = 23;
+        assert happyBirthday(gifts).equals("Happy Birthday Jhandu!!!") ;
+        System.out.println("All test cases in main function passed");
+
+    }
+}
