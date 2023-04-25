fde9b6feb844032466ecdaff443d644b50ca262c
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-25 20:07:59 +0530

    Update Solution_same.java

--- DSA - 2/Session 4/1. Find the equal partition index/Solution_same.java
+++ DSA - 2/Session 4/1. Find the equal partition index/Solution_same.java
@@ -1,18 +1,18 @@
 // Time : O(n)
 // Space: O(n)
 static int equalPartition(int n, long arr[]) {
-    long suffix [] = new long [n];
-        suffix[n-1]  = arr[n-1];
-        for(int i=n-2; i >= 0; i--){
-            suffix[i] = suffix[i+1] + arr[i];
-        }
-
-        long prefixSum = 0;
-        for(int i=1; i<n-1; i++){
-            prefixSum += arr[i-1];
-            if(prefixSum == suffix[i+1]){
-                return i;
-            }
-        }
-        return -1;
+long suffix [] = new long [n];
+    suffix[n-1]  = arr[n-1];
+    for(int i=n-2; i >= 0; i--){
+        suffix[i] = suffix[i+1] + arr[i];
     }
+
+    long prefixSum = 0;
+    for(int i=1; i<n-1; i++){
+        prefixSum += arr[i-1];
+        if(prefixSum == suffix[i+1]){
+            return i;
+        }
+    }
+    return -1;
+}
