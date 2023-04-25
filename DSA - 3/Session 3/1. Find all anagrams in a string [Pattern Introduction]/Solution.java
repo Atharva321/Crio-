2d65ee205ec1c0ae79d0a56a3c9bc9f64ae9affd
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-26 00:53:45 +0530

    Update Solution.java

--- DSA - 3/Session 3/1. Find all anagrams in a string [Pattern Introduction]/Solution.java
+++ DSA - 3/Session 3/1. Find all anagrams in a string [Pattern Introduction]/Solution.java
@@ -112,19 +112,3 @@ class FindAllAnagramsInAString {
 
         // return ans;
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-	    String[] input = new String[2]; 
-	    input = scanner.nextLine().split(" ");
-        String s = input[0];
-        String p = input[1];
-        scanner.close();
-
-        List<Integer> result = new FindAllAnagramsInAString().findAnagrams(s,p);
-	    System.out.println(result.size());
-        for (Integer i = 0; i < result.size(); ++i) {
-            System.out.printf("%d ", result.get(i));
-        }
-    }
-}
